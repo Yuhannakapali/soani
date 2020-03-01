3afbb440cce31cfb31dc1a3fb4dfec2583594d83
yuhannakapali <yuhannakapali@gmail.com>
2020-03-01 12:10:56 +0545

    added og tags for fb share

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
 
+    @yield('tag')
+    
     @yield('title')
     
     @include('frontend.layout.stylesheet')
