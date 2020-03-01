ef1753a64cb23feb9a00c99b762f61a9f505b76d
yuhannakapali <yuhannakapali@gmail.com>
2020-03-01 12:56:58 +0545

    author bug fix

--- app/Http/Controllers/blogsController.php
+++ app/Http/Controllers/blogsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Blog;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 class blogsController extends Controller
 {   
     protected $dir= 'images/upload';
