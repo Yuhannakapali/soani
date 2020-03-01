02a417fa46eae4d281e714f29c67be5fcb9628ba
yuhannakapali <yuhannakapali@gmail.com>
2020-03-01 12:48:00 +0545

    tags bug test

--- resources/views/frontend/section/tags.blade.php
+++ resources/views/frontend/section/tags.blade.php
@@ -1,7 +1,9 @@
-
-
+@if (!empty($datas))
+@foreach ($datas as $data)
 <meta property="og:url"           content="{!!'yuhannakapali.com.np/blog/'.$data->title!!}" />
 <meta property="og:type"          content="website" />
 <meta property="og:title"         content="{{$data->title}}" />
 <meta property="og:description"   content=" {!! ucfirst(substr($data->body,0,450)) !!}" />
 <meta property="og:image"         content="{{asset('images/upload/'.$data->image_name)}}" />
+@endforeach    
+@endif
