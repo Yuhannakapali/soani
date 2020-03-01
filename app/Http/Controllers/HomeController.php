d1a7d6691f7fdba9778fe49f8d1769b03314f69e
yuhannakapali <yuhannakapali@gmail.com>
2020-03-01 16:30:21 +0545

    change in /home routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.blog.index');
+        return view('admin.layout.newdashboard.master');
     }
 }
