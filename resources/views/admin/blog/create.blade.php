3dbc558ab653676dcc8188855828b730267dbb35
yuhannakapali <yuhannakapali@gmail.com>
2020-03-04 15:08:23 +0545

    blog bug fixed

--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -11,7 +11,7 @@
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="/home">Home</a></li>
-          <li class="breadcrumb-item active">Blog</li>
+          <li class="breadcrumb-item active">Blogs</li>
         </ol>
       </div>
     </div>
