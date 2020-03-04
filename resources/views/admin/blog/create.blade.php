48a1119e3f021e5ea7a5572a568e20f6d5757b30
yuhannakapali <yuhannakapali@gmail.com>
2020-03-04 17:01:51 +0545

    blog ceated form added

--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -1,88 +1,69 @@
-@extends('admin.layout.newdashboard.master') 
-
-
-@section('content')
+@extends('admin.layout.newdashboard.master') @section('content')
 <section class="content-header">
-  <div class="container-fluid">
-    <div class="row mb-2">
-      <div class="col-sm-6">
-        <h1>Blog</h1>
-      </div>
-      <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="/home">Home</a></li>
-          <li class="breadcrumb-item active">Blogs</li>
-        </ol>
-      </div>
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Blog</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="/home">Home</a></li>
+                    <li class="breadcrumb-item active">Blogs</li>
+                </ol>
+            </div>
+        </div>
     </div>
-  </div><!-- /.container-fluid -->
+    <!-- /.container-fluid -->
 </section>
 <section class="content">
     <div class="container-fluid">
-      <div class="row ">
-        <div class="col-md-9">
-          <div class="card card-primary card-outline">
-            <div class="card-header">
-              <h3 class="card-title">Compose New Message</h3>
+        <div class="row ">
+            <div class="col-md-9">
+                <form
+                    class="form-validate"
+                    method="post"
+                    enctype="multipart/form-data"
+                    action="{{ route('Blog.store') }}"
+                >
+                    @include('admin.forms.blog_form')
+                </form>
             </div>
+            <!-- /.col -->
 
-              <div>
-                <form 
-              class="form-validate"
-              method="post"
-              enctype="multipart/form-data"
-              action="{{route('Blog.store')}}">
-                  @include('admin.forms.blog_form')
-            </form>
-              </div>
-            <div class="card-footer">
-              <div class="float-right">
-                <button type="button" class="btn btn-default"><i class="fas fa-pencil-alt"></i> Draft</button>
-                <button type="submit" class="btn btn-primary"><i class="far fa-envelope"></i> Send</button>
-              </div>
-              <button type="reset" class="btn btn-default"><i class="fas fa-times"></i> Discard</button>
+            <div class="col-3">
+                <form
+                    class="form-validate"
+                    method="post"
+                    action="{{ route('Category.store') }}"
+                >
+                    @include('admin.forms.category_form')
+                </form>
             </div>
-            <!-- /.card-footer -->
-          </div>
-          <!-- /.card -->
         </div>
-        <!-- /.col -->
-        
-        <div class="col-3">
-          <form
-          class="form-validate"
-          method="post" 
-          action="{{route('Category.store')}}"> 
-                @include('admin.forms.category_form')
-          </form>
-        </div>
-        
-      </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-  </section>
-  <!-- /.content -->
-@endsection
-
-@section('javascript')
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+@endsection @section('javascript')
 <script>
-      var msg = "{{ Session()->get('message') }}"
-     var cla = "{{ Session()->get('alert-class') }}"  
-     var condition = "{{ Session::has('message') }}"
-  $(function() {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000
-      });
-      if (condition) {
-        console.log(msg);
-        Toast.fire({
-          type: cla,
-          title: msg
-          
-        })
-      }  
+    var msg = "{{ Session()->get('message') }}";
+    var cla = "{{ Session()->get('alert-class') }}";
+    var condition = "{{ Session::has('message') }}";
+    $(function() {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000
+        });
+        if (condition) {
+            console.log(msg);
+            Toast.fire({
+                type: cla,
+                title: msg
+            });
+        }
     });
-    </script>  
+</script>
 @endsection
