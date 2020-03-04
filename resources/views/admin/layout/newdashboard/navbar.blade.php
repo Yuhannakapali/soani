fd603e5f032fe4f4fb9bebc9f696b2e1983574e9
yuhannakapali <yuhannakapali@gmail.com>
2020-03-04 14:33:35 +0545

    nav bar right control removed

--- resources/views/admin/layout/newdashboard/navbar.blade.php
+++ resources/views/admin/layout/newdashboard/navbar.blade.php
@@ -59,11 +59,6 @@
             </li>
         </ul>
     </li> --}}
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
