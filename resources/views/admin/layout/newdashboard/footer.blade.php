a5b0b3718d49cc6e2b0fa70adea5c340bf1f6e12
yuhannakapali <yuhannakapali@gmail.com>
2020-03-12 15:19:19 +0545

    footer link to soanitech

--- resources/views/admin/layout/newdashboard/footer.blade.php
+++ resources/views/admin/layout/newdashboard/footer.blade.php
@@ -2,6 +2,6 @@
     {{-- <div class="float-right d-none d-sm-block">
       <b>Version</b> 
     </div> --}}
-    <strong>Copyright &copy; 2020 <a href="http://adminlte.io">SoAniTech</a>.</strong> All rights
+    <strong>Copyright &copy; 2020 <a href="http://soanitech.com">SoAniTech</a>.</strong> All rights
     reserved.
   </footer>
