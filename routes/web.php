afaaac06997778fa09f8c47bd8d768a23e351f4c
yuhannakapali <yuhannakapali@gmail.com>
2020-03-12 15:32:19 +0545

    registration disbable

--- routes/web.php
+++ routes/web.php
@@ -93,7 +93,7 @@ Route::resource('Blog', 'blogsController')->middleware('auth');
 //Category
 Route::resource('Category', 'categoryController')->middleware('auth');
 
-Auth::routes();
+Auth::routes(['register'=> false ]);
 //logout
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
