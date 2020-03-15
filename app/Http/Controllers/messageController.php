058ae20fc5c8580675ec2d46c5c607e4073e8162
yuhannakapali <yuhannakapali@gmail.com>
2020-03-15 15:13:02 +0545

    captcha disable for now

--- app/Http/Controllers/messageController.php
+++ app/Http/Controllers/messageController.php
@@ -15,58 +15,82 @@ class MessageController extends Controller
     }
    
     public function store(Request $request){
-        
-        $url = 'https://www.google.com/recaptcha/api/siteverify';
-        $remoteip = $_SERVER['REMOTE_ADDR'];
-        $data = [
-        'secret' => env('CAPTCHA_SECRET'),
-        'response' => $request->get('token'),
-        'remoteip' => $remoteip
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email ',
+            'message'=>'required',
+
+        ]);
+        $mail = new Message();
+        $mail->name = $request->name;
+        $mail->phone = $request->phone;
+        $mail->email = $request->email;
+        $mail->message = $request->message;
+        $mail->save();
+
+        $details = [
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'message' => $request->message,
         ];
-        $options = [
-            'http' => [
-            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method' => 'POST',
-            'content' => http_build_query($data)
-            ]
-        ];
-        $context = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        $resultJson = json_decode($result);
+
+        Mail::send(new contact($details));
+
         
-        if ($resultJson->success != true) {
-            return back()->withErrors(['captcha' => 'go away bot ']);
-        }
-        elseif ($resultJson->score >= 0.3) {
+    return back()->with('message', 'Thanks for your message!');
+
+        // $url = 'https://www.google.com/recaptcha/api/siteverify';
+        // $remoteip = $_SERVER['REMOTE_ADDR'];
+        // $data = [
+        // 'secret' => env('CAPTCHA_SECRET'),
+        // 'response' => $request->get('token'),
+        // 'remoteip' => $remoteip
+        // ];
+        // $options = [
+        //     'http' => [
+        //     'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        //     'method' => 'POST',
+        //     'content' => http_build_query($data)
+        //     ]
+        // ];
+        // $context = stream_context_create($options);
+        // $result = file_get_contents($url, false, $context);
+        // $resultJson = json_decode($result);
+        
+        // if ($resultJson->success != true) {
+        //     return back()->withErrors(['captcha' => 'go away bot ']);
+        // }
+        // elseif ($resultJson->score >= 0.3) {
             
-            $request->validate([
-                    'name' => 'required',
-                    'email' => 'required|email ',
-                    'message'=>'required',
+        //     $request->validate([
+        //             'name' => 'required',
+        //             'email' => 'required|email ',
+        //             'message'=>'required',
         
-                ]);
-                $mail = new Message();
-                $mail->name = $request->name;
-                $mail->phone = $request->phone;
-                $mail->email = $request->email;
-                $mail->message = $request->message;
-                $mail->save();
+        //         ]);
+        //         $mail = new Message();
+        //         $mail->name = $request->name;
+        //         $mail->phone = $request->phone;
+        //         $mail->email = $request->email;
+        //         $mail->message = $request->message;
+        //         $mail->save();
         
-                $details = [
-                    'name' => $request->name,
-                    'phone' => $request->phone,
-                    'email' => $request->email,
-                    'message' => $request->message,
-                ];
+        //         $details = [
+        //             'name' => $request->name,
+        //             'phone' => $request->phone,
+        //             'email' => $request->email,
+        //             'message' => $request->message,
+        //         ];
         
-                Mail::send(new contact($details));
+        //         Mail::send(new contact($details));
         
                 
-            return back()->with('message', 'Thanks for your message!');
-        } 
-        else {
-            return back()->withErrors(['captcha' => 'ReCaptcha Error']);
-        }
+        //     return back()->with('message', 'Thanks for your message!');
+        // } 
+        // else {
+        //     return back()->withErrors(['captcha' => 'ReCaptcha Error']);
+        // }
     }
     
     public function index()
